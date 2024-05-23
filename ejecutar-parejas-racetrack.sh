05d13cabb253347e89b5c0afe6010d371c7d881b
Qifan Wang <wwangqifan46@gmail.com>
2024-05-23 19:04:25 +0200

    ejecutar-parejas-racetrack.sh

--- ejecutar-parejas-racetrack.sh
+++ ejecutar-parejas-racetrack.sh
@@ -1,3 +1,4 @@
+
 #!/bin/bash
 
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
@@ -13,24 +14,32 @@ exit
                                           --mem-config configuraciones-sample/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfigs/ctxconfig.gcc-xalancbmk \
 --x86-min-inst-per-ctx 5000 --epoch-length 100 \
-                                          --reports-dir reports-dir-gcc-xalancbmk
+--own-report reports-dir/gcc-xalancbmk/data-racetrack.csv \
+--rtmi-report reports-dir/gcc-xalancbmk/inst-racetrack.csv \
+                                          --reports-dir reports-dir/gcc-xalancbmk
 
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config configuraciones-sample/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config configuraciones-sample/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfigs/ctxconfig.lbm-gcc \
 --x86-min-inst-per-ctx 5000 --epoch-length 100 \
-                                          --reports-dir reports-dir-lbm-gcc
+--own-report reports-dir/lbm-gcc/data-racetrack.csv \
+--rtmi-report reports-dir/lbm-gcc/inst-racetrack.csv \
+                                          --reports-dir reports-dir/lbm-gcc
 
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config configuraciones-sample/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config configuraciones-sample/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfigs/ctxconfig.lbm-mcf \
 --x86-min-inst-per-ctx 5000 --epoch-length 100 \
-                                          --reports-dir reports-dir-lbm-mcf
+--own-report reports-dir/lbm-mcf/data-racetrack.csv \
+--rtmi-report reports-dir/lbm-mcf/inst-racetrack.csv \
+                                          --reports-dir reports-dir/lbm-mcf
 
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config configuraciones-sample/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config configuraciones-sample/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfigs/ctxconfig.lbm-xalancbmk \
 --x86-min-inst-per-ctx 5000 --epoch-length 100 \
-                                          --reports-dir reports-dir-lbm-xalancbmk
+--own-report reports-dir/lbm-xalancbmk/data-racetrack.csv \
+--rtmi-report reports-dir/lbm-xalancbmk/inst-racetrack.csv \
+                                          --reports-dir reports-dir/lbm-xalancbmk
 
 
