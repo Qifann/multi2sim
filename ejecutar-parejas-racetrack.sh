065fee3c33a41a37d60d3b4b2f84aee2986a49f3
qifan <wangqifan46@gmail.com>
2025-05-26 13:31:53 +0200

    Erratas y ctx a 500000000 instr.

--- ejecutar-parejas-racetrack.sh
+++ ejecutar-parejas-racetrack.sh
@@ -12,41 +12,41 @@ mkdir -p reports-racetrack/$TIMESTAMP
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
 					  --ctx-config ctxconfig/ctxconfig.gcc-mcf \
---x86-min-inst-per-ctx 5000 --epoch-length 100   \
---own-report reports-dir/gcc-mcf/data-racetrack.csv \
---rtmi-report reports-dir/gcc-mcf/inst-racetrack.csv \
+--x86-min-inst-per-ctx 500000000 --epoch-length 100   \
+--own-report reports-racetrack/$TIMESTAMP/reports-dir-gcc-mcf/data-racetrack.csv \
+--rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-gcc-mcf/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-gcc-mcf &
 
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.gcc-xalancbmk \
---x86-min-inst-per-ctx 5000 --epoch-length 100 \
---own-report reports-dir/gcc-xalancbmk/data-racetrack.csv \
---rtmi-report reports-dir/gcc-xalancbmk/inst-racetrack.csv \
+--x86-min-inst-per-ctx 500000000 --epoch-length 100 \
+--own-report reports-racetrack/$TIMESTAMP/reports-dir-gcc-xalancbmk/data-racetrack.csv \
+--rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-gcc-xalancbmk/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-gcc-xalancbmk &
 
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-gcc \
---x86-min-inst-per-ctx 5000 --epoch-length 100 \
---own-report reports-dir/lbm-gcc/data-racetrack.csv \
---rtmi-report reports-dir/lbm-gcc/inst-racetrack.csv \
+--x86-min-inst-per-ctx 500000000 --epoch-length 100 \
+--own-report reports-racetrack/$TIMESTAMP/reports-dir-lbm-gcc/data-racetrack.csv \
+--rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-lbm-gcc/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-lbm-gcc &
 
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-mcf \
---x86-min-inst-per-ctx 5000 --epoch-length 100 \
---own-report reports-dir/lbm-mcf/data-racetrack.csv \
---rtmi-report reports-dir/lbm-mcf/inst-racetrack.csv \
+--x86-min-inst-per-ctx 500000000 --epoch-length 100 \
+--own-report reports-racetrack/$TIMESTAMP/reports-dir-lbm-mcf/data-racetrack.csv \
+--rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-lbm-mcf/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-lbm-mcf &
 
  multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-xalancbmk \
---x86-min-inst-per-ctx 5000 --epoch-length 100 \
---own-report reports-dir/lbm-xalancbmk/data-racetrack.csv \
---rtmi-report reports-dir/lbm-xalancbmk/inst-racetrack.csv \
+--x86-min-inst-per-ctx 500000000 --epoch-length 100 \
+--own-report reports-racetrack/$TIMESTAMP/reports-dir-lbm-xalancbmk/data-racetrack.csv \
+--rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-lbm-xalancbmk/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-lbm-xalancbmk
 
 
