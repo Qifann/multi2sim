6d4d9750e62f01667832d52a37827a7cace19ce4
qifan <wangqifan46@gmail.com>
2025-07-09 12:01:49 +0200

    Ajustes slurm.

--- ejecutar-parejas-racetrack.sh
+++ ejecutar-parejas-racetrack.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH -n 5 -N 5
-#SBATCH -o slurm_output/slurm-%j.out
+#SBATCH --ntasks 29
+#SBATCH --cpus-per-task=1
 
 TIMESTAMP=$(date '+%Y-%m-%d_%H:%M:%S')
 
@@ -8,7 +8,7 @@ mkdir -p reports-racetrack/$TIMESTAMP
 
 
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
 					  --ctx-config ctxconfig/ctxconfig.gcc-mcf \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000   \
@@ -16,7 +16,7 @@ mkdir -p reports-racetrack/$TIMESTAMP
 --rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-gcc-mcf/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-gcc-mcf &
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.gcc-xalancbmk \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000 \
@@ -24,7 +24,7 @@ mkdir -p reports-racetrack/$TIMESTAMP
 --rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-gcc-xalancbmk/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-gcc-xalancbmk &
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-gcc \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000 \
@@ -32,7 +32,7 @@ mkdir -p reports-racetrack/$TIMESTAMP
 --rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-lbm-gcc/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-lbm-gcc &
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-mcf \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000 \
@@ -40,7 +40,7 @@ mkdir -p reports-racetrack/$TIMESTAMP
 --rtmi-report reports-racetrack/$TIMESTAMP/reports-dir-lbm-mcf/inst-racetrack.csv \
                                           --reports-dir reports-racetrack/$TIMESTAMP/reports-dir-lbm-mcf & 
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_combined_160kb_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-xalancbmk \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000 \
--- ejecutar-parejas.sh
+++ ejecutar-parejas.sh
@@ -1,36 +1,37 @@
 #!/bin/bash
-#SBATCH -n 5 -N 5
+#SBATCH --ntasks 5
+#SBATCH --cpus-per-task=1
 #SBATCH -o slurm_output/slurm-%j.out
 
 TIMESTAMP=$(date '+%Y-%m-%d_%H:%M:%S')
 
 mkdir -p reports/$TIMESTAMP
 
-multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.gcc-mcf \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000   \
                                           --reports-dir reports/$TIMESTAMP/reports-dir-gcc-mcf &
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.gcc-xalancbmk \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000 \
                                           --reports-dir reports/$TIMESTAMP/reports-dir-gcc-xalancbmk &
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-gcc \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000 \
                                           --reports-dir reports/$TIMESTAMP/reports-dir-lbm-gcc &
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-mcf \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000 \
                                           --reports-dir reports/$TIMESTAMP/reports-dir-lbm-mcf &
 
- multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
+srun multi2sim-4.1/bin/m2s --x86-sim detailed --x86-config cpuconfig/cpuconfig_baseline_1core_SMT2 \
                                           --mem-config memconfig/memconfig_baseline_1core_SMT2 \
                                           --ctx-config ctxconfig/ctxconfig.lbm-xalancbmk \
 --x86-min-inst-per-ctx 500000000 --epoch-length 10000000 \
